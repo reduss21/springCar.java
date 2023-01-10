c05f017c11e3bfce13785566a1b51cb6d6682d81
reduss21 <kartosferon@gmail.com>
2023-01-10 23:26:18 +0300

    beep-beep created

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,33 +1,46 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.sql.SQLException;
-import java.util.List;
+import javax.persistence.NoResultException;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+   public static void main(String[] args) {
+      AnnotationConfigApplicationContext context =
+              new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User user1 = new User("name", "lastname", "com@alt.com");
+      User user2 = new User("noname", "nolast", "nocom@noalt.nocom");
+      User user3 = new User("wat", "da", "dog@doing.com");
+      User user4 = new User("raib", "shydow", "lupinds@po.com");
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+      Car car1 = new Car("alpha", 2021);
+      Car car2 = new Car("betta", 1001);
+      Car car3 = new Car("gamma", 7);
+      Car car4 = new Car("delta", 290);
+
+      userService.add(user1.setCar(car1).setUser(user1));
+      userService.add(user2.setCar(car2).setUser(user2));
+      userService.add(user3.setCar(car3).setUser(user3));
+      userService.add(user4.setCar(car4).setUser(user4));
+
+      for (User user : userService.listUsers()) {
+         System.out.println(user + " " + user.getCar());
+         System.out.println("----отделение----строки----");
       }
+      System.out.println(userService.getUserByCar("betta", 1001));
+      System.out.println("----отделение----строки----");
+        try {
+           User notFoundUser = userService.getUserByCar("gag", 90);
+      } catch (NoResultException e) {
+           System.out.println("User not found");
+           System.out.println("----отделение----строки----");
+        }
 
       context.close();
    }
